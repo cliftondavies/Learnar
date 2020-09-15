670c133668b6c149483de9e0464371da92469311
cliftondavies <clifton.davies@outlook.com>
2020-09-16 00:10:43 +0100

    Add validations and associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { in: 6..16 }
+  validates :username, presence: true, length: { in: 4..15 }, uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true
+
+  has_many :incomings, class_name: 'Following', foreign_key: :followed_id, inverse_of: :followed,
+                       dependent: :destroy
+  has_many :outgoings, class_name: 'Following', foreign_key: :follower_id, inverse_of: :follower,
+                       dependent: :destroy
+
+  has_many :followers, through: :incomings
+  has_many :followeds, through: :outgoings
+
+  has_many :progress_updates, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end

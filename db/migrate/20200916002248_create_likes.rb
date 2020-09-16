ca6d3b1f33fe97d200d7ef80c486d56e32ed06a7
cliftondavies <clifton.davies@outlook.com>
2020-09-16 11:57:14 +0100

    Fix creation of likes table

--- db/migrate/20200916002248_create_likes.rb
+++ db/migrate/20200916002248_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :likes do |t|
-      t.foreign_key :users
-      t.foreign_key :progress_updates
+      t.references :user, foreign_key: true
+      t.references :progress_update, foreign_key: true
       t.index [:user_id, :progress_update_id], unique: true
       t.timestamps
     end

9467a3b4fe2bda1553c715c54c465292d654e8e3
cliftondavies <clifton.davies@outlook.com>
2020-09-16 22:41:47 +0100

    Add #user_photo helper to display gravatar for user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_photo(user, options = { size: 64, class: 'mr-3' })
+    photo_id = Digest::MD5::hexdigest(user.email.downcase)
+    photo_size = options[:size]
+    image_class = options[:class]
+    photo_url = "https://secure.gravatar.com/avatar/#{photo_id}?s=#{photo_size}"
+    image_tag(photo_url, alt: 'User photo', class: image_class)
+  end
 end

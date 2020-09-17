fb119c56e63cbd9ab1693cfbdc3544a591366e35
cliftondavies <clifton.davies@outlook.com>
2020-09-17 23:58:24 +0100

    Define routes for users, followings and progress_updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
+
+  devise_for :users
+
+  resources :users, only: [:show] do
+    resources :followings, only: %i[create destroy]
+  end
+
+  resources :progress_updates, only: %i[index create]
 end

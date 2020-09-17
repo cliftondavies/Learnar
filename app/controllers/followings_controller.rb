65af566e59ca9f35479ba312326ba64efc705abc
cliftondavies <clifton.davies@outlook.com>
2020-09-17 23:46:35 +0100

    Update #destroy in followings_controller

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -14,7 +14,12 @@ class FollowingsController < ApplicationController
 
   def destroy
     @following = Following.find(params[:id])
-    @following.destroy
-    redirect_back fallback_location: user_path(current_user), notice: "You unfollowed #{@following.followed.name}!"
+    if @following
+      @following.destroy
+      redirect_back fallback_location: user_path(current_user), notice: "You unfollowed #{@following.followed.name}!"
+    else
+      redirect_back fallback_location: user_path(current_user),
+                    alert: 'You cannot unfollow someone you did not previously follow.'
+    end
   end
 end

719ba0e171007aced0dc6919aef270e5a69505ea
cliftondavies <clifton.davies@outlook.com>
2020-09-23 22:06:52 +0100

    Clean up application code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   before_save :format_input
 
   devise :database_authenticatable, :registerable,
